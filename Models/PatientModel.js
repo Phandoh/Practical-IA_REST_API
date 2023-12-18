8b5dd9b45aa4b910e2ab074cadd6e0749c0bda6e
Jan Phandoh <janphandoh@gmail.com>
2023-12-18 08:34:11 +0000

    Create a database model for patient

---
+++ Models/PatientModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const patientSchema = mongoose.Schema(
+    {
+        Patient_ID: {
+            type: Number,
+            required: true
+        },
+        Surname: {
+            type: String,
+            required: true
+        },
+        Othernames: {
+            type: String,
+            required: true
+        },
+        Gender: {
+            type: String,
+            required: true
+        },
+        PhoneNumber: {
+            type: Number,
+            required: true
+        },
+        ResidentialAddress: {
+            type: String,
+            required: true
+        },
+        EmergencyName: {
+            type: String,
+            required: true
+        },
+        Contact: {
+            type: Number,
+            required: true
+        },
+        RelationshipWithPatient: {
+            type: String,
+            required: true
+        }
+    },{
+        timestamps: true
+    }
+);
+
+const Patient = mongoose.model("Patient",patientSchema)
+module.exports = Patient;
